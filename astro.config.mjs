79cbf31ad74e0826144557bdc69a5ec55b2a8d6e
roemdev <emivirdisla@gmail.com>
2026-01-07 22:37:26 -0400

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://emivir-dev.vercel.app',
-  base: '/', 
+  base: 'emivir-dev.vercel.app', 
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
